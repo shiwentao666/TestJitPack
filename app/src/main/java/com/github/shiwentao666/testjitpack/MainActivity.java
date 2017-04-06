4fe040438547867eb497a28da68b0e8166dfcd57
shiwentao666 <shiwento666@gmail.com>
2017-04-06 15:20:57 +0800

    111

---
+++ app/src/main/java/com/github/shiwentao666/testjitpack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.shiwentao666.testjitpack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ jitlib/src/main/java/com/github/shiwentao666/jitlib/TestJit.java
@@ -0,0 +1,14 @@
+package com.github.shiwentao666.jitlib;
+
+/**
+ * Title: TestJit <br>
+ * Description: <br>
+ * Copyright (c) 传化物流版权所有 2017 <br>
+ * Created DateTime: 2017/4/6 15:13
+ * Created by Wentao.Shi.
+ */
+public class TestJit {
+    public static void ttt(){
+        System.out.println("ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt");
+    }
+}
